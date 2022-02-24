9b4f665fbe7eb676a41aa0b39308f678f74351dd
amberrr0420 <amberrr0420@gmail.com>
2022-02-24 15:13:26 +0800

    feat: add login & welcome hbs

--- app.js
+++ app.js
@@ -9,8 +9,32 @@ const Account = require('./models/account')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// routes
 app.get('/', (req,res) => {
   res.render('login')
+})
+
+app.post('/login', (req,res) => {
+  const { email, password } = req.body
+  console.log(email, password)
+  Account.findOne({ email })
+    .lean()
+    .then(user => {
+      console.log(user)
+      if(!user) {
+        res.render('login', { noUser: "no user" })
+      } else if(user.password === password) {
+        res.render('welcome', { user })
+      } else {
+        res.render('login', { invalidPassword: "invalid PW" })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.listen(3004, () => {
