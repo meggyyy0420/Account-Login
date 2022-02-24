52d2a207e1fc053345472d8ba0478a67ab58e9ae
amberrr0420 <amberrr0420@gmail.com>
2022-02-24 11:12:25 +0800

    feat: account model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 require('./config/mongoose')
+const Accounts = require('./models/account')
 
 app.get('/', (req,res) => {
   res.send('Account Login')
