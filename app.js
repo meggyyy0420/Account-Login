86eae267a1c8e2026325540214f62d756a07c6b5
amberrr0420 <amberrr0420@gmail.com>
2022-02-27 00:30:16 +0800

    feat: add cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 // connect with mongodb
 require('./config/mongoose')
 
@@ -12,6 +13,9 @@ app.set('view engine', 'hbs')
 
 // body-parser
 app.use(express.urlencoded({ extended: true }))
+
+// cookie-parser
+app.use(cookieParser('123456789'))
 
 // routes
 app.use(routes)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,49 @@
 const express = require('express')
 const router = express.Router()
 
+const Account = require('../../models/account')
+
 router.get('/', (req, res) => {
-  res.render('login')
+  let firstName = ''
+  let isLogin = false
+  if(req.signedCookies.firstName) {
+    firstName = req.signedCookies.firstName
+    isLogin = true
+  }
+  console.log(firstName)
+  res.render('index',{ firstName, isLogin })
+})
+
+router.post('/login', (req, res) => {
+  const { firstName, email, password } = req.body
+  let isLogin = false
+  console.log(email, password)
+  Account.findOne({ email })
+    .lean()
+    .then(user => {
+      console.log(user)
+      if (!user) {
+        res.render('index', { isLogin, noUser: "no user" })
+      } else if (user.password === password) {
+        isLogin = true
+        res.cookie('firstName', user.firstName, {
+          path: '/',
+          signed: true,
+          maxAge: 60000
+        })
+        res.render('index', { isLogin, firstName: user.firstName })
+      } else {
+        res.render('index', { isLogin, invalidPassword: "invalid PW" })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+router.post('/logout', (req,res) => {
+  res.clearCookie('firstName', { path: '/' })
+  return res.redirect('/')
 })
 
 module.exports = router
